1416f22e36fb90385e94cf76348c5fb29f88a19f
jdiez17 <jose.manuel.diez@gmail.com>
2013-07-14 22:41:12 +0200

    Replaced sequential daemon with process pool

--- daemon.py
+++ daemon.py
@@ -2,14 +2,15 @@ from gifquick.worker import process_gif
 from gifquick.database import r, _k
 
 import time
+import multiprocessing 
 
 if __name__ == '__main__':
+    pool = multiprocessing.Pool()
+
     while True:
         while r.llen(_k("gifqueue")):
             job = r.lpop(_k("gifqueue")) 
-            process_gif(job)
-
-            print "Processed", job
+            pool.apply_async(process_gif, args=(job,))
        
         print "Sleeping" 
         time.sleep(1)
