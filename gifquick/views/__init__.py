445924d07e82b978cc2aa5f0d488183f7b9c26de
jdiez17 <jose.manuel.diez@gmail.com>
2013-07-18 16:53:54 +0200

    Removed obsolete file

--- gifquick/views/__init__.py
+++ gifquick/views/__init__.py
@@ -1,4 +1,8 @@
-from gif import GifView 
-from image import ImageView
-from api import APIView
-from hook import HookView
+__all__ = ['gif', 'image', 'api', 'hook']
+
+from . import *
+
+#from gif import GifView 
+#from image import ImageView
+#from api import APIView
+#from hook import HookView
