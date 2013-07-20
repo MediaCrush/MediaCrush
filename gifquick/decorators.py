567eaeb9518d3cc464175070f85433556f8cda17
jdiez17 <jose.manuel.diez@gmail.com>
2013-07-20 23:39:30 +0200

    Added JSONP support to all the API

--- gifquick/decorators.py
+++ gifquick/decorators.py
@@ -1,13 +1,21 @@
-from flask import jsonify
+from flask import jsonify, request
 from functools import wraps
 
 def json_output(f):
     @wraps(f)
     def wrapper(*args, **kwargs):
+        def jsonify_wrap(obj):
+            callback = request.args.get('callback', False) 
+            jsonification = jsonify(obj)
+            if callback:
+                jsonification.data = "%s(%s);" % (callback, jsonification.data) # Alter the response
+
+            return jsonification
+
         result = f(*args, **kwargs)
         if isinstance(result, tuple):
-            return jsonify(result[0]), result[1]
-        return jsonify(result)
+            return jsonify_wrap(result[0]), result[1]
+        return jsonify_wrap(result)
 
 
     return wrapper
