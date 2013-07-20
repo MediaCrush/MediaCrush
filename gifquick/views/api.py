2c59c1028af1f0f2342f4477f3bfaf5c8dbf09b2
jdiez17 <jose.manuel.diez@gmail.com>
2013-07-20 23:29:47 +0200

    Added multi-hash API endpoint

--- gifquick/views/api.py
+++ gifquick/views/api.py
@@ -1,4 +1,5 @@
 from flask.ext.classy import FlaskView, route
+from flask import request
 
 from ..decorators import json_output
 from ..files import media_url, get_mimetype, extension, processing_needed
@@ -15,13 +16,8 @@ class APIView(FlaskView):
             'file': media_url(f),
         }
 
-
-    @route("/api/<id>.json")
-    @route("/<id>.json")
-    @json_output
-    def get(self, id):
-        f = File.from_hash(id) 
-
+    @staticmethod
+    def _file_object(f):
         if not f.original:
             return {'error': 404}, 404
 
@@ -37,7 +33,36 @@ class APIView(FlaskView):
              
         if ext in processing_needed:
             for f_ext in processing_needed[ext]['formats']:
-                ret['files'].append(APIView._file_entry("%s.%s" % (id, f_ext)))
+                ret['files'].append(APIView._file_entry("%s.%s" % (f.hash, f_ext)))
 
         ret['files'].append(APIView._file_entry(f.original))
         return ret
+
+    @route("/api/<id>")
+    @route("/<id>.json")
+    @json_output
+    def get(self, id):
+        f = File.from_hash(id) 
+
+        if not f.original:
+            return {'error': 404}, 404
+
+        return APIView._file_object(f)
+
+    @route("/api/info")
+    @json_output
+    def info(self):
+        if not "list" in request.args:
+            return {'error': 400}, 400
+        items = request.args['list'].split(',')
+
+        res = {}
+        for i in items:
+            f = File.from_hash(i)
+            
+            if not f.original:
+                res[i] = None
+            else:
+                res[i] = APIView._file_object(f)
+        
+        return res
