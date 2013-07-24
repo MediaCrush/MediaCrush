9f5388f8398c7a6b3c9df40169a973cb6e8c51a6
Drew DeVault <sir@cmpwn.com>
2013-07-24 07:12:45 -0700

    Fix update hook

--- mediacrush/views/hook.py
+++ mediacrush/views/hook.py
@@ -9,6 +9,7 @@ from ..network import *
 
 class HookView(FlaskView):
     def post(self):
+        print 'Hook recieved'
         allow = False
         for ip in _cfg("hook_ips").split(","):
             parts = ip.split("/")
@@ -19,14 +20,17 @@ class HookView(FlaskView):
             if addressInNetwork(dottedQuadToNum(request.remote_addr), addr):
                 allow = True
         if not allow:
+            print 'Hook ignored - not whitelisted IP'
             abort(403)
+        print 'Hook permitted'
         # Pull and restart site
         event = json.loads(request.form["payload"])
-        if not _cfg("hook_repository") == "%s/%s" % (json["repository"]["owner"]["name"], json["repository"]["name"]):
+        if not _cfg("hook_repository") == "%s/%s" % (event["repository"]["owner"]["name"], event["repository"]["name"]):
             return "ignored"
         if any("[noupdate]" in c["message"] for c in event["commits"]):
             return "ignored"
         if "refs/heads/" + _cfg("hook_branch") == event["ref"]:
+            print 'Updating on hook'
             call(["git", "pull", "origin", "master"])
             call(_cfg("restart_command").split())
             return "thanks"
