2413330771d8c1953049281caab5a79b073b102c
Ahmed Azaan <azaan@outlook.com>
2013-08-11 02:23:30 +0530

    add cookie remove function, if days 0 set to expire

--- static/app.js
+++ static/app.js
@@ -4,8 +4,19 @@ function createCookie(name,value,days) {
         date.setTime(date.getTime()+(days*24*60*60*1000));
         var expires = "; expires="+date.toGMTString();
     }
-    else var expires = "";
+    else var expires = "; expires=Thu, 01-Jan-1970 00:00:01 GMT";
     document.cookie = name+"="+value+expires+"; path=/";
+}
+
+function readCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
 }
 
 function adOptOut() {
