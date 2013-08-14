d712709cb2abf3d4cdda01b96e95173dda8f2926
jdiez17 <jose.manuel.diez@gmail.com>
2013-08-14 18:33:10 +0200

    [noupdate] Added markdown renderer to docs

--- mediacrush/views/__init__.py
+++ mediacrush/views/__init__.py
@@ -2,3 +2,4 @@ from upload import UploadView
 from image import ImageView
 from api import APIView
 from hook import HookView
+from docs import DocsView
---
+++ mediacrush/views/docs.py
@@ -0,0 +1,23 @@
+from flask.ext.classy import FlaskView
+from flask import render_template, abort
+
+from ..config import _cfg
+
+import os
+
+class DocsView(FlaskView):
+    @staticmethod
+    def _get_doc(id):
+        with open(os.path.join(_cfg("docs_path"), id + ".md")) as f:
+            content = ''.join(f.readlines())
+            return render_template("doc.html", content=content)
+
+
+    def index(self):
+        return DocsView._get_doc('index')
+
+    def get(self, doc):
+        try:
+            return DocsView._get_doc(doc) 
+        except:
+            abort(404)
