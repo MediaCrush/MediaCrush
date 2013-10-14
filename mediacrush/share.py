10466c00c16cc51b7f8e4bac313123b7b3d79f85
jdiez17 <jose.manuel.diez@gmail.com>
2013-10-14 13:47:42 +0200

    Slightly refactored share.py

--- mediacrush/share.py
+++ mediacrush/share.py
@@ -12,37 +12,43 @@ def _still_image(h):
 
 class Share(object):
     def __call__(self, method, h):
-        return getattr(self, method)(h)
+        original = _still_image(h)
+        if original:
+            method += "_still"
+            arg = original
+        else:
+            method += "_other"
+            arg = h
 
-    def link(self, h):
+        return getattr(self, method)(arg)
+
+    def link_still(self, h):
         return domain_url(h) 
-    
-    def directlink(self, h):
-        original = _still_image(h)
-        if original:
-            return domain_url(original) 
 
-        return self.link(h) + "/direct"
+    link_other = link_still
 
-    def markdown(self, h):
-        original = _still_image(h)
-        if original:
-            return "![](%s)" % domain_url(original)
+    def directlink_still(self, h):
+        return domain_url(h) 
 
+    def directlink_other(self, h):
+        return self.link_other(h) + "/direct"
+
+    def markdown_still(self, h):
+        return "![](%s)" % domain_url(h)
+
+    def markdown_other(self, h):
         return "[MediaCrush](%s)" % domain_url(h)
 
-    def html(self, h):
-        original = _still_image(h)
-        if original:
-            return "<img src='%s'>" % domain_url(original)
+    def html_still(self, h):
+        return "<img src='%s'>" % domain_url(h)
 
+    def html_other(self, h):
         return "<a href='%s'>MediaCrush</a>" % domain_url(h)
 
-    def bbcode(self, h):
-        original = _still_image(h)
-        if original:
-            return "[img]%s[/img]" % domain_url(original)
+    def bbcode_still(self, h):
+        return "[img]%s[/img]" % domain_url(h)
 
+    def bbcode_other(self, h):
         return "[url=%s]MediaCrush[/url]" % domain_url(h)
 
 share = Share()
