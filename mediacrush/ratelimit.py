c2a30ae236b686a6b74c1c3cf15b622e97ac4cd3
jdiez17 <jose.manuel.diez@gmail.com>
2013-10-30 11:28:21 +0100

    Revamped ratelimit system

--- mediacrush/ratelimit.py
+++ mediacrush/ratelimit.py
@@ -1,25 +1,22 @@
-from .config import _cfg, _cfgi
+from .config import config
 from .database import r, _k
 from .network import get_ip
 
 from flask import request
 
 
-def rate_limit_exceeded():
-    consumed = int(r.get(_k("rate_limit.%s" % get_ip())))
+def rate_limit_exceeded(section="upload"):
+    consumed = int(r.get(_k("rate_limit.%s.%s" % (section, get_ip()))))
+    print consumed
     if not consumed:
         return False
 
-    return consumed >= _cfgi("bytes_per_hour")
+    return consumed >= config.getint('ratelimit-%s' % section, 'units_per_period') 
 
-
-def rate_limit_update(f):
-    key = _k("rate_limit.%s" % get_ip())
-    f.seek(0, 2)
-    b = f.tell()
-    f.seek(0)
+def rate_limit_update(by, section="upload"):
+    key = _k("rate_limit.%s.%s" % (section, get_ip()))
 
     if not r.exists(key):
-        r.setex(key, 3600, b)
+        r.setex(key, config.get('ratelimit-%s' % section, 'period'), by)
     else:
-        r.incrby(key, b)
+        r.incrby(key, by)
