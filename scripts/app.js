62daf6e0a3a954113acb095d2963dd95c11aaaed
Drew DeVault <sir@cmpwn.com>
2013-11-14 20:08:36 -0800

    Finished up album creation UI

--- scripts/app.js
+++ scripts/app.js
@@ -6,6 +6,7 @@ function browse() {
 var firstUpload = true;
 var uploads = 0;
 var files = [];
+var albumAssociated = false;
 
 function uploadUrl(url) {
     var droparea = document.getElementById('droparea');
@@ -21,6 +22,7 @@ function uploadUrl(url) {
     preview.fileStatus.appendChild(p);
     preview.progress.style.width = '100%';
     uploads++;
+    updateAlbum();
     var xhr = new XMLHttpRequest();
     xhr.open('POST', '/api/upload/url');
     xhr.onload = function() {
@@ -29,6 +31,7 @@ function uploadUrl(url) {
             p.textContent = 'Processing... (this may take a while)';
             preview.fileStatus.appendChild(p);
             hash = responseJSON['hash'];
+            files.push(hash);
             preview.progress.className += ' progress-green';
             preview.progress.style.width = '100%';
             setTimeout(function() {
@@ -58,6 +61,7 @@ function handleFiles(files) {
     var timeout = 500;
     for (var i = 0; i < files.length; i++) {
         uploads++;
+        updateAlbum();
         if (i == 0)
             handleFile(files[i]);
         else {
@@ -68,6 +72,46 @@ function handleFiles(files) {
             }(i);
             timeout += 500;
         }
+    }
+}
+
+function createAlbum(e) {
+    e.preventDefault();
+    albumAssociated = true;
+    document.getElementById('createAlbum').className = 'hidden';
+    document.getElementById('albumPending').classList.remove('hidden');
+}
+
+function updateAlbum() {
+    if (!albumAssociated) return;
+    if (files.length == 0) {
+        document.getElementById('createAlbum').className.add('hidden');
+        document.getElementById('albumPending').classList.add('hidden');
+        document.getElementById('albumUrl').parentElement.classList.add('hidden');
+    }
+    if (uploads > 0) {
+        document.getElementById('createAlbum').className = 'hidden';
+        document.getElementById('albumPending').classList.remove('hidden');
+        document.getElementById('albumUrl').parentElement.classList.add('hidden');
+    } else {
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST', '/api/album/create');
+        xhr.onload = function() {
+            if (this.status != 200) {
+                document.getElementById('albumPending').textContent = 'An error occured creating this album.';
+                return;
+            }
+            var data = JSON.parse(this.responseText);
+            document.getElementById('createAlbum').className = 'hidden';
+            document.getElementById('albumPending').classList.add('hidden');
+            var url = document.getElementById('albumUrl');
+            url.textContent = window.location.origin + '/' + data.hash;
+            url.href = window.location.origin + '/' + data.hash;
+            url.parentElement.classList.remove('hidden');
+        };
+        var form = new FormData();
+        form.append('list', files.join(','));
+        xhr.send(form);
     }
 }
 
@@ -83,6 +127,8 @@ function handleFile(file) {
             error.className = 'error';
             error.textContent = 'This filetype is not supported.';
             preview.fileStatus.appendChild(error);
+            files.remove(files.indexOf(hash));
+            updateAlbum();
         } else {
             var xhr = new XMLHttpRequest();
             xhr.open('GET', '/api/' + hash + '/exists');
@@ -103,6 +149,7 @@ function handleFile(file) {
                     preview.fileStatus.appendChild(a);
                     preview.fileStatus.parentElement.appendChild(a2);
                     uploads--;
+                    updateAlbum();
                     addItemToHistory(hash);
                 } else {
                     var p = document.createElement('p');
@@ -154,6 +201,8 @@ function uploadFile(file, hash, statusUI, progressUI) {
             errorText.textContent = error;
             statusUI.appendChild(errorText);
             progressUI.style.width = 0;
+            files.remove(files.indexOf(hash));
+            updateAlbum();
         }
     };
     var formData = new FormData();
@@ -169,6 +218,7 @@ function checkStatus(hash, statusUI, progressUI) {
         if (responseJSON['status'] == 'done') {
             progressUI.parentElement.removeChild(progressUI);
             finish(statusUI, hash);
+            updateAlbum();
         } else if (responseJSON['status'] == 'timeout' || responseJSON['status'] == 'error') {
             progressUI.parentElement.removeChild(progressUI);
             var error = document.createElement('p');
@@ -180,7 +230,9 @@ function checkStatus(hash, statusUI, progressUI) {
                 error.textContent = 'There was an error processing this file.';
             }
             statusUI.appendChild(error);
+            files.remove(files.indexOf(hash));
             uploads--;
+            updateAlbum();
         } else {
             setTimeout(function() {
                 checkStatus(hash, statusUI, progressUI);
@@ -338,11 +390,13 @@ function dropEnable() {
         e.preventDefault();
         browse();
     }, false);
+    var create = document.getElementById('createAlbum');
+    create.addEventListener('click', createAlbum, false);
     setTimeout(handleHistory, 50);
 }
 
 function forceFocus() {
-    if (document.activeElement.tagName == 'TEXTAREA') {
+    if (document.activeElement.tagName == 'TEXTAREA' || document.activeElement.tagName == 'INPUT') {
         setTimeout(forceFocus, 250);
         return;
     }
