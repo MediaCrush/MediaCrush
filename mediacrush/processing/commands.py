f66c1637ea375597712afb21778d98782eeaf90d
jdiez17 <jose.manuel.diez@gmail.com>
2013-12-02 19:23:34 +0000

    Add  file

---
+++ mediacrush/processing/commands.py
@@ -0,0 +1,4 @@
+from mediacrush.processing.utils import Invocation
+
+mp4 = Invocation("ffmpeg -i {0} -vcodec libx264 -pix_fmt yuv420p -vf scale=trunc(in_w/2)*2:trunc(in_h/2)*2 {1}.mp4")
+webm = Invocation("ffmpeg -i {0} -c:v libvpx -c:a libvorbis -pix_fmt yuv420p -quality good -b:v 2M -crf 5 {1}.webm")
