89688fe678d2a91eee4625d49c8dc6b5dbb5d0b5
José Manuel Díez <jose.manuel.diez@gmail.com>
2013-12-12 13:48:16 +0000

    Move copy operation to the top so ffmpeg overwrites the file only (instead of cp overwriting it).

--- mediacrush/processing/processors.py
+++ mediacrush/processing/processors.py
@@ -8,10 +8,10 @@ class VideoProcessor(Processor):
     extras = ['png']
 
     def sync(self):
+        self._execute(copy)
         self._execute("ffmpeg -i {0} -vframes 1 {1}.png")
         self._execute("ffmpeg -i {0} -vcodec libx264 -pix_fmt yuv420p -vf scale=trunc(in_w/2)*2:trunc(in_h/2)*2 {1}.mp4")
         self._execute("ffmpeg -i {0} -c:v libvpx -c:a libvorbis -pix_fmt yuv420p -quality good -b:v 2M -crf 5 {1}.webm")
-        self._execute(copy)
 
     def async(self):
         self._execute("ffmpeg -i {0} -q 5 -pix_fmt yuv420p -acodec libvorbis -vcodec libtheora {1}.ogv")
