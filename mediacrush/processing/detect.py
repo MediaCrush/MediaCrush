ea72790537bfc3e42a4f65ea453f8a59f58a775b
Drew DeVault <sir@cmpwn.com>
2014-02-06 03:57:17 -0800

    Add meaningful metadata from ffprobe

--- mediacrush/processing/detect.py
+++ mediacrush/processing/detect.py
@@ -49,7 +49,9 @@ def detect_ffprobe(path):
         return None
     result = json.loads(a.stdout[0])
     if result["format"]["nb_streams"] == 1:
-        return detect_stream(result["streams"][0])
+        detected = detect_stream(result["streams"][0])
+        detected['metadata'] = ffprobe_addExtraMetadata(detected['metadata'], result)
+        return detected
     audio_streams = 0
     video_streams = 0
     image_streams = 0
@@ -58,8 +60,16 @@ def detect_ffprobe(path):
     # We shouldn't penalize people for unknown streams, I just figured we could make a note of it
     unknown_streams = 0
 
+    metadata = dict()
+
     for stream in result["streams"]:
         s = detect_stream(stream)
+        # Set up some metadata
+        if s['metadata'] != None:
+            if 'duration' in s['metadata']:
+                metadata['duration'] = s['metadata']['duration']
+            if 'dimensions' in s['metadata']:
+                metadata['dimensions'] = s['metadata']['dimensions']
         t = s['type']
         if not s or not t:
             unknown_streams += 1
@@ -76,18 +86,23 @@ def detect_ffprobe(path):
                 font_streams += 1
             else:
                 unknown_streams += 1
+    metadata = ffprobe_addExtraMetadata(metadata, result)
     if audio_streams == 1 and video_streams == 0:
+        metadata['has_audio'] = True
+        metadata['has_video'] = False
         return {
             'type': 'audio',
             'processor_state': { 'has_audio': True, 'has_video': False },
-            'metadata': None,
+            'metadata': metadata,
             'flags': None
         }
     if video_streams > 0:
+        metadata['has_audio'] = audio_streams > 0
+        metadata['has_video'] = True
         return {
             'type': 'video',
             'processor_state': { 'has_audio': audio_streams > 0, 'has_video': True },
-            'metadata': None,
+            'metadata': metadata,
             'flags': {
                 'autoplay': False,
                 'loop': False,
@@ -95,6 +110,19 @@ def detect_ffprobe(path):
             }
         }
     return None
+
+def ffprobe_addExtraMetadata(metadata, result):
+    if 'format' in result:
+        f = result['format']
+        if 'tags' in f:
+            t = f['tags']
+            if 'ALBUM' in t:
+                metadata['album'] = t['ALBUM']
+            if 'COMPOSER' in t:
+                metadata['composer'] = t['COMPOSER']
+            if 'TITLE' in t:
+                metadata['title'] = t['TITLE']
+    return metadata
 
 def detect_stream(stream):
     if not "codec_name" in stream:
@@ -110,21 +138,21 @@ def detect_stream(stream):
         if stream["codec_name"] == 'mjpeg':
             return {
                 'type': 'image/jpeg',
-                'metadata': None,
+                'metadata': { 'dimensions': { 'width': int(stream['width']), 'height': int(stream['height']) } },
                 'processor_state': None,
                 'flags': None
             }
         if stream["codec_name"] == 'png':
             return {
                 'type': 'image/png',
-                'metadata': None,
+                'metadata': { 'dimensions': { 'width': int(stream['width']), 'height': int(stream['height']) } },
                 'processor_state': None,
                 'flags': None
             }
         if stream["codec_name"] == 'webp':
             return {
                 'type': 'image',
-                'metadata': None,
+                'metadata': { 'dimensions': { 'width': int(stream['width']), 'height': int(stream['height']) } },
                 'processor_state': None,
                 'flags': None
             }
@@ -133,7 +161,7 @@ def detect_stream(stream):
         if stream["codec_name"] == 'gif':
             return {
                 'type': 'video',
-                'metadata': None,
+                'metadata': { 'has_audio': False, 'has_video': True, 'dimensions': { 'width': int(stream['width']), 'height': int(stream['height']) } },
                 'processor_state': { 'has_audio': False, 'has_video': True },
                 'flags': {
                     'autoplay': True,
@@ -144,7 +172,7 @@ def detect_stream(stream):
     if stream["codec_type"] == 'video':
         return {
             'type': 'video',
-            'metadata': None,
+            'metadata': { 'dimensions': { 'width': int(stream['width']), 'height': int(stream['height']) } },
             'processor_state': { 'has_audio': False, 'has_video': True },
             'flags': {
                 'autoplay': False,
@@ -155,7 +183,7 @@ def detect_stream(stream):
     if stream["codec_type"] == 'audio':
         return {
             'type': 'audio',
-            'metadata': None,
+            'metadata': { 'duration': float(stream["duration"]) },
             'processor_state': { 'has_audio': True, 'has_video': False },
             'flags': None
         }
