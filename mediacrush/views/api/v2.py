ffc5b93bfb64b060e4cff720def0d875a4f1be91
Drew DeVault <sir@cmpwn.com>
2014-05-03 16:57:51 -0600

    Update APIv2 to support more file querying stuff

--- mediacrush/views/api/v2.py
+++ mediacrush/views/api/v2.py
@@ -3,7 +3,7 @@ from flask.ext.classy import FlaskView, route
 from mediacrush.decorators import json_output
 from mediacrush.processing import get_processor
 from mediacrush.fileutils import normalise_processor
-from mediacrush.objects import RedisObject, File
+from mediacrush.objects import RedisObject, File, FailedFile
 from mediacrush.files import media_url, get_mimetype
 
 import json
@@ -17,6 +17,7 @@ def _file_object(f):
     metadata = {}
     if f.metadata and f.metadata != 'None':
         metadata = json.loads(f.metadata)
+
     ret = {
         'original': media_url(f.original),
         'blob_type': normalise_processor(f.processor),
@@ -25,9 +26,14 @@ def _file_object(f):
         'extras': [],
         'metadata': metadata,
         'flags': f.flags.as_dict(),
+        'status': f.status
     }
     if f.compression:
         ret['compression'] = float(f.compression)
+    if f.status in ['error', 'timeout', 'unrecognized']:
+        # TODO: We should not use magic strings here, but instead we should modify the processing pipeline
+        ret['status'] = 'error'
+        ret['error'] = f.status
 
     ret['files'].append(_file_entry(f.original, mimetype=f.mimetype))
 
@@ -56,15 +62,19 @@ class APIv2(FlaskView):
     decorators = [json_output] # TODO: `cors_v2`
 
     def get(self, q):
-        hashes = q.split("+")
+        hashes = q.split(",")
         ret = []
 
         for hash in hashes:
-            cls = RedisObject.klass(hash)
-            if cls not in _objects:
+            klass = RedisObject.klass(hash)
+            if not klass:
                 return NOT_FOUND
 
-            obj = cls.from_hash(hash)
-            ret.append(_objects[cls](obj))
+            obj = klass.from_hash(hash)
+            if klass is FailedFile:
+                # TODO: Composing this manually may not be the best idea
+                ret.append({ "status": "error", "hash": hash, "error": obj.status })
+            else:
+                ret.append(_objects[klass](obj))
 
         return {'list': ret}
