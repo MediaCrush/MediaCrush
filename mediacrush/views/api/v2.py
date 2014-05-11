6df8a6ee29bccc4d3f7c8db447bea49b303da33e
jdiez17 <jose.manuel.diez@gmail.com>
2014-05-11 17:32:12 +0100

    Added delete to v2

--- mediacrush/views/api/v2.py
+++ mediacrush/views/api/v2.py
@@ -1,14 +1,19 @@
 from flask.ext.classy import FlaskView, route
+from flaskext.bcrypt import check_password_hash
 
 from mediacrush.decorators import json_output
 from mediacrush.processing import get_processor
 from mediacrush.fileutils import normalise_processor
-from mediacrush.objects import RedisObject, File, FailedFile
-from mediacrush.files import media_url, get_mimetype
+from mediacrush.objects import RedisObject, File, FailedFile, Album
+from mediacrush.files import media_url, get_mimetype, delete_file
+from mediacrush.network import get_ip
 
 import json
 
-NOT_FOUND = {'status': 'not_found'}, 404
+NOT_FOUND = lambda hash: {"hash": hash, "result": "not_found"}
+UNAUTHORIZED = lambda hash: {"hash": hash, "result": "unauthorized"}
+SUCCESS = lambda hash: {"hash": hash, "result": "success"}
+OBJ_ERROR = lambda obj: {"hash": obj.hash, "result": obj.status}
 
 def _file_object(f):
     mimetype = f.mimetype
@@ -61,6 +66,11 @@ _objects = {
     File: _file_object
 }
 
+_deletion_funcs = {
+    File: delete_file,
+    Album: lambda a: a.delete()
+}
+
 class APIv2(FlaskView):
     decorators = [json_output] # TODO: `cors_v2`
 
@@ -71,13 +81,36 @@ class APIv2(FlaskView):
         for hash in hashes:
             klass = RedisObject.klass(hash)
             if not klass:
-                return NOT_FOUND
+                ret.append(NOT_FOUND(hash))
+                continue
 
             obj = klass.from_hash(hash)
             if klass is FailedFile:
-                # TODO: Composing this manually may not be the best idea
-                ret.append({ "status": "error", "hash": hash, "error": obj.status })
+                ret.append(OBJ_ERROR(obj))
             else:
                 ret.append(_objects[klass](obj))
+
+        return {'result': ret}
+
+    def delete(self, q):
+        hashes = q.split(",")
+        ret = []
+
+        for hash in hashes:
+            klass = RedisObject.klass(hash)
+
+            if not klass:
+                ret.append(NOT_FOUND(hash))
+                continue
+
+            try:
+                o = klass.from_hash(hash)
+                if not check_password_hash(o.ip, get_ip()):
+                    ret.append(UNAUTHORIZED(hash))
+
+                _deletion_funcs[klass](o)
+                ret.append(SUCCESS(hash))
+            except:
+                ret.append(UNAUTHORIZED(hash))
 
         return {'result': ret}
