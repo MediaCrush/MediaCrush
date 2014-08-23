be2a463e08234843a2bbb9e6882ff0d89288f00c
jdiez17 <jose.manuel.diez@gmail.com>
2014-08-23 19:10:20 +0200

    Revert "Added error logging"
    This reverts commit ae05f5770b15c144d5aba5d0fc7dc86f5af2b7f9.

--- mediacrush/processing/invocation.py
+++ mediacrush/processing/invocation.py
@@ -1,15 +1,13 @@
-from mediacrush.config import _cfgi, _cfg
+from mediacrush.config import _cfgi
 
 import os
 import threading
 import subprocess
-import time
 
 class Invocation(object):
     crashed = False
     exited = False
     stdout = None
-    stderr = None
     process = None
     args = []
 
@@ -23,7 +21,7 @@ class Invocation(object):
     def _target(self):
         try:
             self.process = subprocess.Popen(self.args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            self.stdout, self.stderr = self.process.communicate()
+            self.stdout = self.process.communicate()
         except:
             self.crashed = True
             return
@@ -43,12 +41,3 @@ class Invocation(object):
             self.exited = True
 
         self.returncode = self.process.returncode
-        if self.returncode != 0:
-            if _cfg("error_folder"):
-                now = int(time.time())
-                with open(os.path.join(_cfg("error_folder"), "error-log-%s.txt" % now), "w") as f:
-                    f.write(" ".join(self.args) + "\n\n")
-
-                    f.write(self.stdout)
-                    f.write(self.stderr)
-                    f.flush()
