fb3782f66c7cb8bfb668b8903ed62335e0807e46
jdiez17 <jose.manuel.diez@gmail.com>
2014-08-24 22:55:07 +0200

    Specify static folder

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from mediacrush.app import app
 from mediacrush.config import _cfg, _cfgi
+import os
+
+app.static_folder = os.path.join(os.getcwd(), "static")
 
 if __name__ == '__main__':
     app.run(host=_cfg("debug-host"), port=_cfgi('debug-port'), debug=True)
