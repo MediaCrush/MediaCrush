fc9508cebbd92a8f09c0f860d23cb1413d44a20e
Drew DeVault <sir@cmpwn.com>
2014-08-31 19:15:35 -0600

    Another attempt at disabling mobile site

--- mediacrush/app.py
+++ mediacrush/app.py
@@ -38,8 +38,9 @@ def jinja_template_loader():
     mobile = request.user_agent.platform in ['android', 'iphone', 'ipad'] \
            or 'windows phone' in request.user_agent.string.lower() \
            or 'mobile' in request.user_agent.string.lower()
+    mobile = False
     g.mobile = mobile
-    if mobile and False:
+    if mobile:
         app.jinja_loader = ChoiceLoader([
             FileSystemLoader(os.path.join("templates", "mobile")),
             FileSystemLoader("templates"),
