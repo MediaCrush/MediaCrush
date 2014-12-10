a6e6c32fc8455303c44cebdfa7507300d298aa24
Drew DeVault <sir@cmpwn.com>
2014-12-10 08:16:05 -0700

    Add support for ./hash to mcmanage

--- mediacrush/mcmanage/files.py
+++ mediacrush/mcmanage/files.py
@@ -3,10 +3,12 @@ from ..files import delete_file
 
 def files_delete(arguments):
     hash = arguments['<hash>']
+    if hash.startswith("./"):
+        hash = hash[2:]
 
     f = File.from_hash(hash)
     if not f:
-        print("%r is not a valid file." % arguments["<hash>"])
+        print("%r is not a valid file." % hash)
         return
     delete_file(f)
     print("Done, thank you.")
